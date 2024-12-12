4ab8c9bdc39aa7b9341fac2627f6ca42dcc1939d
Lemon7777 <61228902+Lemon7777-p@users.noreply.github.com>
2024-12-12 20:26:46 +0800

    added cat1 for event

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
   titlee: String,
   venueid: String,
+  cat1:String,
   predateE: String, // keep as string due to inconsistent formatting
   desce: String,
   presenterorge: String
