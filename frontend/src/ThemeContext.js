bda68046475392f64c392413d31805e027461d5a
Lemon7777 <61228902+Lemon7777-p@users.noreply.github.com>
2024-12-13 18:10:31 +0800

    Update ThemeContext.js to fix refresh problem

--- frontend/src/ThemeContext.js
+++ frontend/src/ThemeContext.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  // use localStorage
+  const storedTheme = localStorage.getItem('theme') || 'light';
+  const [theme, setTheme] = useState(storedTheme);
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    if (theme === 'dark') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
